78d28346c293691da3ecbbe425ab2f1221e5765c
Victor <47047497+173716414@users.noreply.github.com>
2023-10-25 16:04:52 +0800

    Day15 Java8 Time

---
+++ src/main/java/victor/day15/Java8Time.java
@@ -0,0 +1,62 @@
+package victor.day15;
+
+/**
+ * @Author：Victor_htq
+ * @Package：victor.day15
+ * @Project：JavaFaundamental
+ * @name：Java8Time
+ * @Date：2023/10/25 15:57
+ * @Filename：Java8Time
+ */
+import java.time.*;
+
+public class Java8Time {
+    public static void main(String args[]){
+        Java8Time java8tester = new Java8Time();
+        java8tester.testLocalDateTime();
+        java8tester.testZonedDateTime();
+    }
+
+    public void testLocalDateTime(){
+
+        // 获取当前的日期时间
+        LocalDateTime currentTime = LocalDateTime.now();
+        System.out.println("当前时间: " + currentTime);
+
+        LocalDate date1 = currentTime.toLocalDate();
+        System.out.println("date1: " + date1);
+
+        Month month = currentTime.getMonth();
+        int day = currentTime.getDayOfMonth();
+        int seconds = currentTime.getSecond();
+
+        System.out.println("月: " + month +", 日: " + day +", 秒: " + seconds);
+
+        LocalDateTime date2 = currentTime.withDayOfMonth(10).withYear(2012);
+        System.out.println("date2: " + date2);
+
+        // 12 december 2014
+        LocalDate date3 = LocalDate.of(2014, Month.DECEMBER, 12);
+        System.out.println("date3: " + date3);
+
+        // 22 小时 15 分钟
+        LocalTime date4 = LocalTime.of(22, 15);
+        System.out.println("date4: " + date4);
+
+        // 解析字符串
+        LocalTime date5 = LocalTime.parse("20:15:30");
+        System.out.println("date5: " + date5);
+    }
+    public void testZonedDateTime(){
+
+        // 获取当前时间日期
+        ZonedDateTime date1 = ZonedDateTime.parse("2015-12-03T10:15:30+05:30[Asia/Shanghai]");
+        System.out.println("date1: " + date1);
+
+        ZoneId id = ZoneId.of("Europe/Paris");
+        System.out.println("ZoneId: " + id);
+
+        ZoneId currentZone = ZoneId.systemDefault();
+        System.out.println("当期时区: " + currentZone);
+    }
+}
